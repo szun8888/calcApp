6f118e0e5434377a5e3580d210caf7adddbb6bfd
yukisako <divine_wind_820@yahoo.co.jp>
2017-10-03 19:56:20 +0900

    数値以外のものを入力したときのエラーアラートを追加

--- app/src/main/java/xyz/yukisako/calcapp/MainActivity.java
+++ app/src/main/java/xyz/yukisako/calcapp/MainActivity.java
@@ -2,6 +2,7 @@ package xyz.yukisako.calcapp;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,31 +28,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button = (Button) findViewById(R.id.devide);
         button.setOnClickListener(this);
 
-
     }
 
     @Override
     public void onClick(View v){
-        getNum();
-        Intent intent = new Intent(this,SecondActivity.class);
-        if(v.getId() == R.id.plus) {
-            intent.putExtra("RESULT", num1 + num2);
-        } else if(v.getId() == R.id.minus){
-            intent.putExtra("RESULT", num1 - num2);
-        } else if(v.getId() == R.id.time){
-            intent.putExtra("RESULT", num1 * num2);
-        } else if(v.getId() == R.id.devide){
-            intent.putExtra("RESULT", num1 / num2);
+        if(getNum()) {
+            Intent intent = new Intent(this, SecondActivity.class);
+            if (v.getId() == R.id.plus) {
+                intent.putExtra("RESULT", num1 + num2);
+            } else if (v.getId() == R.id.minus) {
+                intent.putExtra("RESULT", num1 - num2);
+            } else if (v.getId() == R.id.time) {
+                intent.putExtra("RESULT", num1 * num2);
+            } else if (v.getId() == R.id.devide) {
+                intent.putExtra("RESULT", num1 / num2);
+            }
+            startActivity(intent);
         }
-        startActivity(intent);
     }
 
-    void getNum(){
+    boolean getNum(){
         EditText firstText = (EditText) findViewById(R.id.firstnum);
         EditText secondText = (EditText) findViewById(R.id.secondnum);
 
-        num1 = Double.parseDouble(firstText.getText().toString());
-        num2 = Double.parseDouble(secondText.getText().toString());
+        try{
+            num1 = Double.parseDouble(firstText.getText().toString());
+            num2 = Double.parseDouble(secondText.getText().toString());
+            return true;
+        } catch (NumberFormatException e){
+            showAlertDialog();
+            return false;
+        }
+    }
+
+    private void showAlertDialog() {
+        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("エラー");
+        alertDialogBuilder.setMessage("数値以外のものが入力されています．");
+
+        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+        // AlertDialogを作成して表示する
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 
 
